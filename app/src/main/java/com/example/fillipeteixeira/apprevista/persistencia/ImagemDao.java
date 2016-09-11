b5b04f5976e1fc26617cf6b918bf7806a3fe439b
raphaelaguiar <carvalhodeaguiar123@gmail.com>
2016-09-11 11:51:15 -0300

    Implementação a acesso offline

--- app/src/main/java/com/example/fillipeteixeira/apprevista/persistencia/ImagemDao.java
+++ app/src/main/java/com/example/fillipeteixeira/apprevista/persistencia/ImagemDao.java
@@ -6,6 +6,7 @@ import android.content.ContextWrapper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
+import android.support.v4.graphics.BitmapCompat;
 
 import com.example.fillipeteixeira.apprevista.activitys.TelaAbasActivity;
 import com.example.fillipeteixeira.apprevista.persistencia.restful.HttpUtils;
@@ -25,54 +26,21 @@ import cz.msebera.android.httpclient.Header;
 public class ImagemDao extends Dao{
     private static final ImagemDao instancia = new ImagemDao();
 
-    private ImagemDao(){
-
-    }
-
     public static ImagemDao getInstancia() {
         return instancia;
-    }
-
-    private String getFilePath(String dir, String fileName){
-        /*TODO obter nome do cliente para criar a pasta correta do aplicativo*/
-        return Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES + File.separator +
-                "apkRevista"                   + File.separator +
-                dir                            + File.separator +
-                fileName                       + ".png").getAbsolutePath();
-    }
-
-    private File getFile(String dir, String fileName){
-        return new File(getFilePath(dir,fileName));
     }
 
     private Bitmap getLocalImage(String nomeDaRevista, int nPagina){
         return BitmapFactory.decodeFile(getFilePath(nomeDaRevista,Integer.toString(nPagina)));
     }
 
-    private void saveToInternalStorage(String dir, String fileName,Bitmap bitmapImage){
-        File mypath = getFile(dir,fileName);
-        if(!mypath.exists()) {
-            try {
-                mypath.mkdirs();
-                mypath.delete();
-                mypath.createNewFile();
-                FileOutputStream fos = null;
-                try {
-                    fos = new FileOutputStream(mypath);
-                    bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        fos.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    private void saveToExternalStorage(String dir, String fileName,Bitmap bitmapImage){
+        try {
+            FileOutputStream fos = getFileOutputStream(dir,fileName);
+            bitmapImage.compress(Bitmap.CompressFormat.PNG,100,fos);
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -87,6 +55,7 @@ public class ImagemDao extends Dao{
             get("obterImagem", request, aguardar, new AsyncHttpResponseHandler() {
                 @Override
                 public void onSuccess(int state, Header[] headers, byte[] imagem) {
+                    while(retornoGet!=null);
                     retornoGet = BitmapFactory.decodeByteArray(imagem, 0, imagem.length);
                 }
 
@@ -99,7 +68,7 @@ public class ImagemDao extends Dao{
             retornoGet = null;
 
             if(retorno!=null)
-                saveToInternalStorage(nomeDaRevista,Integer.toString(nPagina),retorno);
+                saveToExternalStorage(nomeDaRevista,Integer.toString(nPagina),retorno);
         }
         return retorno;
     }
--- app/src/main/java/com/example/fillipeteixeira/apprevista/persistencia/restful/HttpUtils.java
+++ app/src/main/java/com/example/fillipeteixeira/apprevista/persistencia/restful/HttpUtils.java
@@ -7,11 +7,7 @@ public class HttpUtils {
     private static final String          ENDERECO = "http://apkrevista.hopto.org:8080/apkRevista";
     private static final String          CLIENTE  = "clienteTeste";
     private static final SyncHttpClient client    = new SyncHttpClient();
-
-    static {
-        client.setTimeout(10);
-    }
-
+    
     public static RequestParams getRequestParams(){
         RequestParams retorno = new RequestParams();
         retorno.put("cliente",CLIENTE);
